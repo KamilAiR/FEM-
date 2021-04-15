b50cda64070ca69474594dd5df2a4b9f880cdac1
KamilAiR <kamil.bak2702@gmail.com>
2021-04-15 14:57:41 +0200

    Poczatek

--- Project1/prog1.py
+++ Project1/prog1.py
@@ -1,17 +1,29 @@
 import numpy as np
-#-------------------------------------------zad 3--------------------------------------------------------
+import matplotlib as  mp
+########################    PRE - PROCESSING  ########################
 
+c=0
+f=0
 
-a = np.arange(1,6)
-b = np.arange(5,0, -1) # co 1 w dol
-c = np.zeros([3,2], dtype = int) #tablica zer
-d=2* np.ones((2,3),  dtype = int) # mnozac ones przez 2 bedziemy miec same 2-ki
-e = np.linspace(-90,-70,3,dtype = int)
-f=np.array([[10],[10],[10],[10],[10]])
+x_a = 0
+x_b = 1
 
-X= np.block([[a],[b]])
-B= np.block([[d],[e]])
-C= np.block([[c,B]])
-D= np.block([[X],[C]])
-A= np.block([[D,f]])
-print(A)
+l_wezlow = 4
+
+#wezly= np.array([1, 2, 3, 4, 5])
+
+wezly = np.array([[1, 0],
+                  [2, 1],
+                  [3, 0.5],
+                  [4,0.75]])
+print(wezly)
+
+elementy = np.array([[1, 1, 3],
+                     [2, 4, 2],
+                     [3, 3, 4]])
+
+twb_L = 'D'
+twb_R = 'D'
+
+wwb_L = 0
+wwb_R = 1
