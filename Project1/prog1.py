a797f56309690dcae1808b9a5dc481c135458755
KamilAiR <kamil.bak2702@gmail.com>
2021-04-15 15:33:50 +0200

    Tyle udalo sie na zajeciach

--- Project1/prog1.py
+++ Project1/prog1.py
@@ -16,7 +16,7 @@ wezly = np.array([[1, 0],
                   [2, 1],
                   [3, 0.5],
                   [4,0.75]])
-print(wezly)
+
 
 elementy = np.array([[1, 1, 3],
                      [2, 4, 2],
@@ -27,3 +27,18 @@ twb_R = 'D'
 
 wwb_L = 0
 wwb_R = 1
+
+
+def generujTabliceGeometrii(x_a,x_b, n):
+    odstep = (x_b - x_a) / (n - 1)
+    matrix = np.array([1, x_a])
+
+    for i in range(1, n, 1):
+        matrix = np.block([
+            [matrix],
+            [i+1, i * odstep+x_a],
+        ])
+    return matrix
+
+
+print(generujTabliceGeometrii(1,4,5))
