f545e577c220a0248eb32a2a33dc8b365b47fce0
KamilAiR <kamil.bak2702@gmail.com>
2021-04-29 11:27:52 +0200

    Zmiany po implementacji Fem - lab2

--- Project1/prog1.py
+++ Project1/prog1.py
@@ -1,14 +1,24 @@
+### Lab implementacja FEM -  1  ###
+
+
 import numpy as np
 import matplotlib.pyplot as  plt
+import scipy.integrate as plt
 ########################    PRE - PROCESSING  ########################
 
 c=0
-f=0
+#wymuszenie
+f= lambda x: 0*x
 
 x_a = 0
 x_b = 1
+x = 5 # ilosc wezlow NOODES
 
-l_wezlow = 4
+twb_L = 'D'
+twb_R = 'D'
+
+wwb_L = 0
+wwb_R = 1
 
 #wezly= np.array([1, 2, 3, 4, 5])
 
@@ -27,6 +37,10 @@ twb_R = 'D'
 
 wwb_L = 0
 wwb_R = 1
+
+WB = [{"ind":1, "typ":'D', "wartosc":1},
+      {"ind":2, "typ":'D', "wartosc":2}]
+      {"ind":x, "typ":'D', "wartosc":2}]
 
 ################### Funkcja generujaca tablice ###################
 
@@ -73,3 +87,75 @@ def rysujgeometrie (tab_w):
 # jako wywolanie podajemy tablice wezlow a otrzymujemy wyplotowana geometirie
 rysujgeometrie(tablica_w)
 
+### Lab implementacja FEM -  2  ###
+
+def Alokacja(x):
+    tmp = (x,x)
+    tmp1 = (x,1)
+    A = np.zeros(tmp)
+    b = np.zeros(tmp1)
+    return A, b
+A,b = Alokacja(x)
+print(A)
+print(b)
+
+
+#def Bazowa(x):
+def FunkcjeBazowe(x):
+    #x stpoień funkcji kształtu
+    #zwraca liste funkcji kształtu
+
+    if x==0:
+        f = (lambda x: 1+0*x)
+        df = (lambda x: 0*x)
+
+    elif x==1:
+
+        f = (lambda x: -1/2*x + 1/2, lambda x: 0.5*x + 0.5)
+        df = (lambda x: -1/2 + 0 * x, lambda x: 0.5 + 0 * x )
+
+    #elif n==2:
+
+        #f = (lambda,lambda,lambda)
+    else:
+        raise Exception("Błąd")
+
+    return f,df
+stopien_funkcji_bazowych = 1
+phi,dphi = FunkcjeBazowe(stopien_funkcji_bazowych)
+
+print(phi)
+print(dphi)
+
+xx = np.linspace(-1,1,101)
+plt.plot(xx,phi[0](xx),'r')
+plt.plot(xx,phi[1](xx),'g')
+plt.plot(xx,dphi[0](xx),'b')
+plt.plot(xx,dphi[1](xx),'c')
+plt.show()
+
+#Preprocesing
+
+liczbaElementow = np.shape(elementy)[0]
+for ee in np.arange(0,liczbaElementow):
+
+    elemRowInd = ee
+    elemGlobalInd = elemety[ee,0]
+
+    elemWezel1 = elemety[ee,1]  #indeks wezla poczatkowego elementu ee
+    elemWezel2 = elemety[ee, 2] # indeks wezla koncowego elementu ee
+
+    Ml = np.zeros(stopien_funkcji_bazowych + 1, stopien_funkcji_bazowych + 1)
+
+    def Aij(df_i, df_j, c, f_i, f_j):
+
+        fun_podc = lambda x: -df_i(x)*df_j(x) + c * f_i(x)*f_j(x)
+
+        return fun_podc
+
+    p_a = wezly[elemWezel1-1,1]
+    p_b = wezly[elemWezel2-1,1]
+
+    J = (p_b-p_a)/2
+
+    Ml[0,0] = J * spint.quad(Aij(dphi[0],dphi[0],c,phi[0],phi[0]),-1,1)
